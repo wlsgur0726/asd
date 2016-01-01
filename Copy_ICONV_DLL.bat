70a28e6d3c505b6b4a5c87050a58ae226959e0c3
wlsgur0726 <wlsgur0726@naver.com>
2016-01-02 05:31:01 +0900

    - 외부라이브러리 밖으로 빼는 작업 - 기존 소스들을 한단계 위의 디렉터리로 이동

---
+++ Copy_ICONV_DLL.bat
@@ -0,0 +1,10 @@
+:: %1  :  Platform (Win32 or x64)
+:: %2  :  Configuration (Debug or Release)
+:: %3  :  OutDir
+
+@echo off
+
+echo Copy ICONV DLL
+cd "%~dp0\..\libiconv\windows\msvc2015\%1\%2"
+xcopy /Y *.dll "%3"
+xcopy /Y *.pdb "%3"
