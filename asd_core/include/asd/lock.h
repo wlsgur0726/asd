1a9b55f8dd1dd12c32acb180354d8eb5167a5323
Karpediem <wlsgur0726@naver.com>
2017-03-05 18:46:05 +0900

    빌드에러 수정

--- asd_core/include/asd/lock.h
+++ asd_core/include/asd/lock.h
@@ -1,6 +1,5 @@
 ﻿#pragma once
 #include "asdbase.h"
-#include "exception.h"
 #include <thread>
 #include <mutex>
 
@@ -17,8 +16,9 @@ namespace asd
 
 
 
-	struct NoLock
+	class NoLock
 	{
+	public:
 		asd_DeclareMutexInterface;
 	};
 
@@ -59,23 +59,18 @@ namespace asd
 	{
 		MutexType* m_mutex;
 		int m_recursionCount = 0;
-		int m_recursionLimit;
 
 	public:
 		Lock(REF MutexType& a_mutex,
-			 IN bool a_getLock = true,
-			 IN int a_recursionLimit = 1)
+			 IN bool a_getLock = true)
 			: m_mutex(&a_mutex)
-			, m_recursionLimit(a_recursionLimit)
 		{
-			assert(a_recursionLimit > 0);
 			if (a_getLock)
 				lock();
 		}
 
 		Lock(MOVE Lock<MutexType>&& a_move) asd_noexcept
 		{
-			m_recursionLimit = a_move.m_recursionLimit;
 			m_recursionCount = a_move.m_recursionCount;
 			m_mutex = a_move.m_mutex;
 			a_move.m_mutex = nullptr;
@@ -100,32 +95,20 @@ namespace asd
 	template <typename MutexType>
 	void Lock<MutexType>::lock()
 	{
-		if (m_recursionLimit > m_recursionCount + 1) {
-			assert(false);
-			throw asd::Exception("락 중첩 한계에 달했습니다.");
-		}
-
 		m_mutex->lock();
 		++m_recursionCount;
 		assert(m_recursionCount > 0);
-		assert(m_recursionCount <= m_recursionLimit);
 	}
 
 	template <typename MutexType>
 	bool Lock<MutexType>::try_lock()
 	{
-		if (m_recursionLimit > m_recursionCount + 1) {
-			assert(false);
-			throw asd::Exception("락 중첩 한계에 달했습니다.");
-		}
-
 		bool r = m_mutex->try_lock();
 		if (r) {
 			++m_recursionCount;
 			assert(m_recursionCount > 0);
 		}
 		assert(m_recursionCount >= 0);
-		assert(m_recursionCount <= m_recursionLimit);
 		return r;
 	}
 
@@ -143,9 +126,8 @@ namespace asd
 
 	template <typename MutexType>
 	inline Lock<MutexType> GetLock(REF MutexType& a_mutex,
-								   IN bool a_getLock = true,
-								   IN int a_recursionLimit = 1)
+								   IN bool a_getLock = true)
 	{
-		return Lock<MutexType>(a_mutex, a_getLock, a_recursionLimit);
+		return Lock<MutexType>(a_mutex, a_getLock);
 	}
 }
